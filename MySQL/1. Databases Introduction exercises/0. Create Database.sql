b1cdd02f415ddbc947d40a7436d2f82515b16f1e
ceco987 <ceco.me4kov@gmail.com>
2020-09-15 20:53:47 +0300

    started exercises

---
+++ MySQL/1. Databases Introduction exercises/0. Create Database.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE `minions`;
+
+USE minions;
---
+++ MySQL/1. Databases Introduction exercises/1. Create Tables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `minions`
+(`id` INT NOT NULL AUTO_INCREMENT,
+`name` VARCHAR(50) NULL,
+`age` INT NOT NULL,
+PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `towns`
+(`town_id` INT NOT NULL AUTO_INCREMENT,
+`name` VARCHAR(50) NOT NULL,
+PRIMARY KEY (`town_id`)
+);
