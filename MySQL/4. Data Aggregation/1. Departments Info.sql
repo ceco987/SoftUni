42329f6a0cb43833516c2f6b49271c3ab9e3694c
ceco987 <ceco.me4kov@gmail.com>
2020-09-29 00:43:07 +0300

    data aggregation lab

---
+++ MySQL/4. Data Aggregation/1. Departments Info.sql
@@ -0,0 +1,4 @@
+SELECT `department_id`, COUNT(`id`) AS`Number of Employees`
+FROM `employees`
+GROUP BY `department_id`
+ORDER BY `department_id`, `Number of Employees`;
---
+++ MySQL/4. Data Aggregation/4. Appetizers Count.sql
@@ -0,0 +1,3 @@
+SELECT COUNT(`id`) AS `Appetizer count` 
+FROM `products`
+WHERE `category_id` =2 AND `price`>8;
