1c1463531ccdfea2e64484085c4926bb9a37d4fa
ceco987 <ceco.me4kov@gmail.com>
2020-12-02 01:53:37 +0200

    spring data 1

---
+++ Spring Data/1. Database Access with JDBC exercise/src/GetMinionsNames.java
@@ -0,0 +1,48 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class GetMinionsNames {
+    public static void main(String[] args) throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+        Setup.setConnection();
+        String statement = "SELECT m.name, m.age\n" +
+                "FROM minions as m\n" +
+                "join minions_villains mv on m.id = mv.minion_id\n" +
+                "join villains v on v.id = mv.villain_id\n" +
+                "where v.id = ?";
+
+        PreparedStatement stmt = Setup.connection.prepareStatement(statement);
+        System.out.print("Enter villain ID: ");
+        int villainID = Integer.parseInt(scanner.nextLine());
+        System.out.println();
+        String villain = findVillain (villainID);
+        if (villain.isBlank()) {
+            System.out.printf("No villain with ID %d exists in the database.",villainID);
+            return;
+        }
+        System.out.println("Villain: "+villain);
+        stmt.setInt(1,villainID);
+        ResultSet rs = stmt.executeQuery();
+        int count = 1;
+        StringBuilder output = new StringBuilder();
+        while(rs.next()){
+            output.append(count++)
+                    .append(". ")
+                    .append(rs.getString("name"))
+                    .append(" ")
+                    .append(rs.getInt ("age"))
+                    .append(System.lineSeparator());
+        }
+        System.out.print(output.toString().trim());
+    }
+
+    private static String findVillain(int villainID) throws SQLException{
+        String findVillain = "SELECT name FROM villains WHERE id = ?";
+        PreparedStatement stmt = Setup.connection.prepareStatement(findVillain);
+        stmt.setInt(1,villainID);
+        ResultSet rs = stmt.executeQuery();
+        return rs.next() ? rs.getString("name") : "";
+    }
+}
---
+++ Spring Data/1. Database Access with JDBC exercise/src/GetVillainsName.java
@@ -0,0 +1,25 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class GetVillainsName {
+    public static void main(String[] args) throws SQLException {
+        Setup.setConnection();
+        String statement = "SELECT v.name, count(*) as count\n" +
+                "FROM villains as v\n" +
+                "join minions_villains mv on v.id = mv.villain_id\n" +
+                "group by v.name\n" +
+                "having count > 15\n" +
+                "order by count desc";
+        PreparedStatement stmt = Setup.connection.prepareStatement(statement);
+        ResultSet rs = stmt.executeQuery();
+        StringBuilder output = new StringBuilder();
+        while (rs.next()) {
+            output.append(rs.getString(1))
+                    .append(" ")
+                    .append(rs.getInt(2))
+                    .append(System.lineSeparator());
+        }
+        System.out.print(output.toString().trim());
+    }
+}
