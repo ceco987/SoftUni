8d0a83ff51b60d1be6b7419e95ce491a87871625
ceco987 <ceco.me4kov@gmail.com>
2021-02-07 17:27:54 +0200

    some of the ex of events

--- JS Advanced/5. Dom Manipulation and Events exercise/01. Sections/script.js
+++ JS Advanced/5. Dom Manipulation and Events exercise/01. Sections/script.js
@@ -1,3 +1,15 @@
 function create(words) {
-   
+    const div = document.getElementById('content');
+    
+    for (let i = 0; i < words.length; i++) {
+        const innerDiv = document.createElement('div');
+        const innerP = document.createElement('p')
+        innerP.textContent=words[i];
+        innerP.style.display = 'none';
+        innerDiv.appendChild(innerP);
+
+        innerDiv.addEventListener('click',(ev)=>ev.target.firstElementChild.style.display='')
+        
+        div.appendChild(innerDiv);
+    }
 }
--- JS Advanced/5. Dom Manipulation and Events exercise/02. Time-Converter/solution.js
+++ JS Advanced/5. Dom Manipulation and Events exercise/02. Time-Converter/solution.js
@@ -1,4 +1,39 @@
 function attachEventsListeners() {
+    document.querySelector('main').addEventListener('click', convertFn);
+    
+    function convertFn(ev) {
+        if (ev.target.type == 'button') {
+            let allTextFields = Array.from(document.querySelectorAll("input[type='text']"));
+            const textField = ev.target.parentNode.querySelector("input[type='text']");
+            const unit = Number(textField.value);
+            const input = textField.id;
+            allTextFields.filter(e=>e.parentNode!=ev.target.parentNode)
+                        .forEach(e=>e.parentNode.querySelector("input[type='text']").value=convert(unit,input,e.id));
 
-    console.log('TODO:...');
+        }
+    }
+
+     function convert(unit, input, output) {
+        unit = Number(unit);
+        input = input.toLowerCase();
+        output = output.toLowerCase();
+
+        const convObj = {
+            days: unit * 86400,
+            hours: unit * 3600,
+            minutes: unit * 60,
+            seconds: unit
+        }
+
+        let outputValue = convObj[input];   
+
+        const finalObj = {
+            days: outputValue / 86400,
+            hours: outputValue / 3600,
+            minutes: outputValue / 60,
+            seconds: outputValue
+        }
+
+        return finalObj[output];
+    }
 }
