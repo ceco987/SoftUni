2867568b7318d4d2fbddb69335c70931456b69dc
ceco987 <ceco.me4kov@gmail.com>
2021-03-10 01:01:45 +0200

    half lab async

--- JS Applications/2. Asynchronous Programming/02.Github-Repos/scripts/app.js
+++ JS Applications/2. Asynchronous Programming/02.Github-Repos/scripts/app.js
@@ -1,5 +1,25 @@
 function loadRepos() {
 	const username = document.getElementById("username").value;
 	const url = `https://api.github.com/users/${username}/repos`;
-	fetch(url).then(response=>response.json().then((data)=>console.log(data)));
+	const repos = document.getElementById("repos");
+	fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			repos.innerHTML = '';
+			document.getElementById("username").value = '';
+			data.forEach(e => {
+				const li = document.createElement("li");
+				const a = document.createElement("a");
+				a.href = e.html_url;
+				a.textContent = e.full_name;
+				li.appendChild(a);
+				repos.appendChild(li);
+			})
+		});
 }
+
+document.getElementById("username").addEventListener("keyup", (ev) => {
+	if (ev.key == "Enter") {
+		document.querySelector("button").click();
+	}
+})
--- JS Applications/2. Asynchronous Programming/03.Gighub-Commits/app.js
+++ JS Applications/2. Asynchronous Programming/03.Gighub-Commits/app.js
@@ -1,4 +1,36 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    const username = document.getElementById("username");
+    const repo = document.getElementById("repo");
+    const url = `https://api.github.com/repos/${username.value}/${repo.value}/commits`;
+    const ul = document.getElementById("commits");
+
+    //fix
+    fetch(url)
+        .then(response => {
+            try {
+                const test = response.json();
+                test.then(data => {
+                    ul.innerHTML = '';
+                    data.forEach(e => {
+                        const li = document.createElement('li')
+                        li.textContent = `${e.commit.author.name}: ${e.commit.message}`;
+                        ul.appendChild(li);
+                    })
+                })
+            }
+            catch (err) {
+                console.log(response)
+                ul.innerHTML = '';
+                const li = document.createElement('li');
+                li.textContent = `Error: ${response.status} (${response.statusText})`;
+                ul.appendChild(li);
+            }
+        })
+
 }
+
+document.querySelector("body").addEventListener('keyup', (ev) => {
+    if (ev.target.tagName == "INPUT" && ev.key == 'Enter') {
+        document.querySelector("button").click();
+    }
+})
