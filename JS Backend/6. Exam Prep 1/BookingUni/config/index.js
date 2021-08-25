c7b61b720ad660a6c532f639b5270b5674714d03
ceco987 <ceco.me4kov@gmail.com>
2021-08-25 15:09:14 +0300

    exam prep

---
+++ JS Backend/6. Exam Prep 1/BookingUni/config/index.js
@@ -0,0 +1,6 @@
+module.exports={
+    PORT: 3000,
+    DB_CONNECTION_STRING: 'mongodb://localhost:27017/booking-uni',
+    TOKEN_SECRET: 'secret token for pass',
+    COOKIE_NAME: 'SESSION_TOKEN'
+}
---
+++ JS Backend/6. Exam Prep 1/BookingUni/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { PORT } = require('./config');
+const databaseConfig = require('./config/database')
+const expressConfig = require('./config/express')
+const routesConfig = require('./config/routes')
+
+start();
+
+async function start() {
+    const app = express();
+    await databaseConfig(app);
+    expressConfig(app);
+    routesConfig(app);
+    app.get('/', (req, res) => res.send('It works!'));
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}...`);
+    })
+}
